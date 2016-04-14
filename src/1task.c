061242aa5c0955b2127db6add3d5b3162a4d32a1
kefir8888 <kefir8888@gmail.com>
2016-04-14 21:51:05 +0300

    :wq B A A

---
+++ src/1task.c
@@ -0,0 +1,33 @@
+#include "sdio.h"
+#include "malloc.h"
+
+typedef int TYPE;
+
+//TODO enum to errors
+//TODO gitlog min 15
+//TODO coverage > 95 %
+//TODO valgrind
+//TODO makefile
+//TODO unittests
+//TODO move massive to library
+
+//#define TYPE   TYPE_INT
+//typedef int TYPE_INT;
+//typedef double TYPE_DOUBLE;
+
+//enum types
+//	{
+//	TYPE_INT,
+//	TYPE_DOUBLE
+//	};
+
+//const char* types_strings [2] = { "int", "double" };
+
+const int DEF_LEN    = 10;
+const int MEM_STEP   = 10;
+const int MAX_DATASZ = 100;
+
+int main ()
+	{
+	return 0;
+	}
