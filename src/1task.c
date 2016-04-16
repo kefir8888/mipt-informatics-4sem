b96a404370c551811406d837c5357180b3d49f8d
kefir8888 <kefir8888@gmail.com>
2016-04-16 11:43:51 +0300

    Added unit-tests with defined check.

--- src/1task.c
+++ src/1task.c
@@ -1,7 +1,10 @@
 #include "stdio.h"
 #include "malloc.h"
+#include "assert.h"
 
 typedef int TYPE;
+
+#define check(COND) { if (COND) { } else { fprintf (stderr, "Condition '%s' is false!\n", #COND); assert (0); } }
 
 //TODO enum to errors  DONE
 //TODO gitlog min 15   DONE
@@ -12,7 +15,7 @@ typedef int TYPE;
 //TODO valgrind        DONE
 // valgrind --leak-check=full --leak-resolution=med ./1task
 //TODO makefile                (?)
-//TODO unittests
+//TODO unittests       DONE
 //TODO move massive to library (?)
 
 const int DEF_LEN    = 10;
@@ -73,27 +76,31 @@ int change_memsz (struct array* inp, int newmemlen)
 	return 1;
 	}
 
-void init_array (struct array* inp)
+int init_array (struct array* inp)
 	{
 	inp -> data    = 0;
 	inp -> memlen  = 0;
 	inp -> datalen = 0;
 	
-	change_memsz (inp, DEF_LEN);
+	int suc = change_memsz (inp, DEF_LEN);
 	
 	arrays_count ++;
+	
+	return suc;
 	}
 
-void delete_array (struct array* inp)
+int delete_array (struct array* inp)
 	{
 	free (inp -> data);
 	inp -> datalen = 0;
 	inp -> memlen  = 0;
 	
 	arrays_count --;
+	
+	return 1;
 	}
 
-void add_element_to_end (struct array* inp, TYPE new_element)
+int add_element_to_end (struct array* inp, TYPE new_element)
 	{
 	int success = 1;
 	
@@ -104,28 +111,38 @@ void add_element_to_end (struct array* inp, TYPE new_element)
 	
 	if (success == 1) inp -> data [inp -> datalen ++] = new_element;
 	else fprintf (stderr, ERRORS [ELEMENT_NOT_ADDED]);
+	
+	return success;
 	}
 
-void remove_element_from_end (struct array* inp)
+int remove_element_from_end (struct array* inp)
 	{
 	inp -> datalen --;
+	
+	return 1;
 	}
 
-void change_element (struct array* inp, int ind, TYPE new_element)
+int change_element (struct array* inp, int ind, TYPE new_element)
 	{
 	if (ind >= inp -> memlen)
+		{
 		printf (ERRORS [WRITE_TO_UNALLOCATED_MEMORY],	ind, inp -> datalen);
-	
-	else inp -> data [ind] = new_element;
+		return 0;
+		}
+		
+	else 	{
+		inp -> data [ind] = new_element;
+		return 1;
+		}
 	}
 
-void zero_array (struct array* inp)
+int zero_array (struct array* inp)
 	{
 	int i = 0;
-	
 	for (; i < inp -> datalen; i ++) inp -> data [i] = 0;
-	
 	inp -> datalen = 0;
+	
+	return 1;
 	}
 
 int get_datalen (struct array* inp)
@@ -138,19 +155,21 @@ int get_memlen (struct array* inp)
 	return inp -> memlen;
 	}
 
-void print_element (struct array* inp, int ind)
+int print_element (struct array* inp, int ind)
 	{
 	if (ind >= inp -> datalen)
 		{
 		printf (ERRORS [GARBAGE_READ], ind, inp -> datalen);
 		
-		return;
+		return 0;
 		}
 	
 	printf ("%i", inp -> data [ind]);
+	
+	return 1;
 	}
 
-void print_array (struct array* inp)
+int print_array (struct array* inp)
 	{
 	int i = 0;
 	
@@ -161,16 +180,19 @@ void print_array (struct array* inp)
 		}
 	
 	printf ("\n");
+	
+	return 1;
 	}
 
-void verbose_full_print (struct array* inp)
+int verbose_full_print (struct array* inp)
 	{
 	printf ("---------------------------------------------------------------\n");
 	printf ("Printing data about obj at %x: \nAllocated memory %i bytes (for %i elements), used %i b, %i el.\n",
 		/*(unsigned int)*/ inp, (inp -> memlen) * sizeof (TYPE), inp -> memlen, (inp -> datalen) * sizeof (TYPE), inp -> datalen);
 	printf ("Printing data in the array:\n");
 	print_array (inp);
-	//printf ("---------------------------------------------------------------\n");
+	
+	return 1;
 	}
 
 int main ()
@@ -178,36 +200,36 @@ int main ()
 	struct array array1;
 	struct array array2;
 	
-	init_array (&array1);
-	init_array (&array2);
+	check (init_array (&array1))
+	check (init_array (&array2))
 	
-	verbose_full_print (&array1);
+	check (verbose_full_print (&array1))
 	
-	add_element_to_end (&array1, 0);
-	add_element_to_end (&array1, 1);
-	add_element_to_end (&array1, 1);
-	add_element_to_end (&array1, 2);
-	add_element_to_end (&array1, 3);
-	add_element_to_end (&array1, 5);
-	add_element_to_end (&array1, 8);
-	add_element_to_end (&array1, 13);
-	verbose_full_print (&array1);
-	remove_element_from_end (&array1);
-	print_array (&array1);
+	check (add_element_to_end (&array1, 0))
+	check (add_element_to_end (&array1, 1))
+	check (add_element_to_end (&array1, 1))
+	check (add_element_to_end (&array1, 2))
+	check (add_element_to_end (&array1, 3))
+	check (add_element_to_end (&array1, 5))
+	check (add_element_to_end (&array1, 8))
+	check (add_element_to_end (&array1, 13))
+	check (verbose_full_print (&array1))
+	check (remove_element_from_end (&array1))
+	check (print_array (&array1))
 	
-	change_element (&array1, 3, 7);
-	print_array (&array1);
+	check (change_element (&array1, 3, 7))
+	check (print_array (&array1))
 	
-	zero_array   (&array1);
-	print_array (&array1);
+	check (zero_array   (&array1))
+	check (print_array (&array1))
 	printf ("datalen = %i, memlen = %i\n", get_datalen (&array1), get_memlen (&array1));
 	
 	for (int i = 0; i < MAX_DATASZ + 5; i ++)
-		add_element_to_end (&array2, 41);
+		/*check (*/add_element_to_end (&array2, 41);//)
 	
-	delete_array (&array1);
+	check (delete_array (&array1))
 	print_exit_message ();
-	delete_array (&array2);
+	check (delete_array (&array2))
 	print_exit_message ();
 	
 	return 0;
